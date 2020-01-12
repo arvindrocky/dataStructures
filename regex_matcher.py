bd203e94b8b80cb598860f473538719bcdaa018e
Arvind Dathathri <arvind.dathathri@gmail.com>
2020-01-12 07:09:20 -0500

    Recursion: Regex Matcher: Updated code to handle multiple * in regex

--- regex_matcher.py
+++ regex_matcher.py
@@ -13,10 +13,7 @@ class Solution:
         if i == len(regex):
             return j == len(input_str)
         if j == len(input_str):
-            if i == len(regex)-1 and regex[i] == "*":
-                return True
-            else:
-                return False
+            return True if len(regex) - i == regex[i:].count("*") else False
         if regex[i] == input_str[j]:
             return self.is_matching_regex_helper(regex, input_str, i+1, j+1)
         if regex[i] == "*":
@@ -31,6 +28,9 @@ print(True == a.is_matching_regex("ab", "ab"))
 print(False == a.is_matching_regex("a", "ab"))
 print(False == a.is_matching_regex("ab", "a"))
 print(True == a.is_matching_regex("a*", "a"))
+print(True == a.is_matching_regex("a**", "a"))
+print(True == a.is_matching_regex("a***", "a"))
+print(False == a.is_matching_regex("a*b", "a"))
 print(False == a.is_matching_regex("a", "ab"))
 print(True == a.is_matching_regex("a*", "abc"))
 print(True == a.is_matching_regex("a*b", "ab"))
