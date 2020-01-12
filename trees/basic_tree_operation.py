98c0a83cc4a137aaca2fa699ca2d6c4f7a00e28f
Arvind Dathathri <arvind.dathathri@gmail.com>
2020-01-12 12:38:43 -0500

    Trees: Added in-order, pre-order and post-order printing of a tree

--- trees/basic_tree_operation.py
+++ trees/basic_tree_operation.py
@@ -17,31 +17,62 @@ class Tree:
         if self.right_node:
             self.right_node.print_tree()
 
+    def print_in_order_tree(self) -> None:
+        if self.left_node:
+            self.left_node.print_in_order_tree()
+        print(self.value)
+        if self.right_node:
+            self.right_node.print_in_order_tree()
 
-root = Tree(10)
+    def print_pre_order_tree(self) -> None:
+        print(self.value)
+        if self.left_node:
+            self.left_node.print_pre_order_tree()
+        if self.right_node:
+            self.right_node.print_pre_order_tree()
+
+    def print_post_order_tree(self) -> None:
+        if self.left_node:
+            self.left_node.print_post_order_tree()
+        if self.right_node:
+            self.right_node.print_post_order_tree()
+        print(self.value)
+
+
+tree = Tree(10)
 print("Printing tree with only root:")
-root.print_tree()
+tree.print_tree()
 
 left_child = Tree(20)
-root.add_child(left_child)
+tree.add_child(left_child)
 
 print("Printing tree with only left child:")
-root.print_tree()
+tree.print_tree()
 
 right_child = Tree(30)
-root.add_child(right_child, False)
+tree.add_child(right_child, False)
 
 print("Printing tree with left and right children:")
-root.print_tree()
+tree.print_tree()
 
 left_grand_child = Tree(40)
 left_child.add_child(left_grand_child)
 
 print("Printing tree with left, right and left grand children:")
-root.print_tree()
+tree.print_tree()
 
 right_grand_child = Tree(50, Tree(60, Tree(70)))
 right_child.add_child(right_grand_child, False)
 
 print("Printing tree with left, right, left grand children and right grand children:")
-root.print_tree()
+tree.print_tree()
+
+another_tree = Tree(10, Tree(20, Tree(40), Tree(50)), Tree(30, Tree(60), Tree(70)))
+print("Printing In Order of a tree:")
+another_tree.print_in_order_tree()
+
+print("Printing In Order of a tree:")
+another_tree.print_pre_order_tree()
+
+print("Printing In Order of a tree:")
+another_tree.print_post_order_tree()
