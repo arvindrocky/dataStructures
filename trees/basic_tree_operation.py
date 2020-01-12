c4dd267c5674c55ad6de4c86f0049ef08d45d720
Arvind Dathathri <arvind.dathathri@gmail.com>
2020-01-12 12:11:05 -0500

    Trees: [1] Added forward referencing [2] Added functionality to add left & right nodes while creating a node [3] Created right grand child by using new constructor

--- trees/basic_tree_operation.py
+++ trees/basic_tree_operation.py
@@ -1,10 +1,10 @@
 class Tree:
-    def __init__(self, value):
-        self.left_node: Tree = None
-        self.right_node: Tree = None
+    def __init__(self, value, left_node: 'Tree' = None, right_node: 'Tree' = None):
+        self.left_node: Tree = left_node
+        self.right_node: Tree = right_node
         self.value: int = value
 
-    def add_child(self, child, is_left_child: bool = True) -> None:
+    def add_child(self, child: 'Tree', is_left_child: bool = True) -> None:
         if is_left_child:
             self.left_node = child
         else:
@@ -38,4 +38,10 @@ left_grand_child = Tree(40)
 left_child.add_child(left_grand_child)
 
 print("Printing tree with left, right and left grand children:")
+root.print_tree()
+
+right_grand_child = Tree(50, Tree(60, Tree(70)))
+right_child.add_child(right_grand_child, False)
+
+print("Printing tree with left, right, left grand children and right grand children:")
 root.print_tree()
