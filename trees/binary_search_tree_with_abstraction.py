8d18d4cdec3d0926dde680b110d5a204ca059eae
Arvind Dathathri <arvind.dathathri@gmail.com>
2020-01-13 19:51:17 -0500

    Trees: BST Operations: Fixed issue with deleting a node

--- trees/binary_search_tree_with_abstraction.py
+++ trees/binary_search_tree_with_abstraction.py
@@ -26,9 +26,10 @@ class BSTOperations:
         if value == node.value:
             return None
         elif value < node.value:
-            BSTOperations.delete_node(node.left_node, value)
+            node.left_node = BSTOperations.delete_node(node.left_node, value)
         else:
-            BSTOperations.delete_node(node.right_node, value)
+            node.right_node = BSTOperations.delete_node(node.right_node, value)
+        return node
 
     @staticmethod
     def print_in_order_of_tree(node: Node) -> None:
@@ -95,13 +96,15 @@ BSTOperations.print_post_order_of_tree(sample_bst)
 
 sample_bst1 = Node(100)
 BSTOperations.insert_node(sample_bst1, 50)
+BSTOperations.insert_node(sample_bst1, 40)
+BSTOperations.insert_node(sample_bst1, 60)
 BSTOperations.insert_node(sample_bst1, 120)
-BSTOperations.insert_node(sample_bst1, 160)
-BSTOperations.insert_node(sample_bst1, 80)
+BSTOperations.insert_node(sample_bst1, 110)
+BSTOperations.insert_node(sample_bst1, 130)
 
 print("Printing In Order of a tree after inserting new nodes:")
 BSTOperations.print_in_order_of_tree(sample_bst1)
 
-BSTOperations.delete_node(sample_bst1, 80)
+BSTOperations.delete_node(sample_bst1, 60)
 print("Printing In Order of a tree after deleting a node:")
 BSTOperations.print_in_order_of_tree(sample_bst1)
