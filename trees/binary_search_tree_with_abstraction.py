b7f19380c541e5cf8fec9b347bbec0af2ab0c46c
Arvind Dathathri <arvind.dathathri@gmail.com>
2020-01-15 12:29:07 -0500

    Trees: [1] Added code to print tree using BST [2] Added code to print all nodes at a given level using queues [3] Added code to print all nodes at a given level using recursion

--- trees/binary_search_tree_with_abstraction.py
+++ trees/binary_search_tree_with_abstraction.py
@@ -1,3 +1,5 @@
+import queue
+
 class Node:
     def __init__(self, value: int, left_child: 'Node' = None, right_child: 'Node' = None):
         self.value: int = value
@@ -99,6 +101,90 @@ class BSTOperations:
         BSTOperations.print_post_order_of_tree(node.right_node)
         print(node.value)
 
+    @staticmethod
+    def print_bst_of_tree(node: Node) -> None:
+        if not node:
+            return
+        bst_queue: queue = queue.Queue(0)
+        bst_queue.put(node)
+        while not bst_queue.empty():
+            current_node: Node = bst_queue.get()
+            print(current_node.value)
+            if current_node.left_node:
+                bst_queue.put(current_node.left_node)
+            if current_node.right_node:
+                bst_queue.put(current_node.right_node)
+
+    @staticmethod
+    def print_bst_of_tree_1(node: Node) -> None:
+        bst_queue: queue = queue.Queue(0)
+        bst_queue.put(node)
+        while not bst_queue.empty():
+            current_node: Node = bst_queue.get()
+            if current_node:
+                print(current_node.value)
+                bst_queue.put(current_node.left_node)
+                bst_queue.put(current_node.right_node)
+
+    @staticmethod
+    def print_all_nodes_at_level_k(node: Node, level: int = 1) -> None:
+        if not node:
+            return
+        if level == 1:
+            print(node.value)
+        else:
+            BSTOperations.print_all_nodes_at_level_k(node.left_node, level - 1)
+            BSTOperations.print_all_nodes_at_level_k(node.right_node, level - 1)
+
+    @staticmethod
+    def print_all_nodes_at_level_k_1(node: Node, level: int = 1, current_level: int = 1) -> None:
+        if not node:
+            return
+        if current_level == level:
+            print(node.value)
+        else:
+            BSTOperations.print_all_nodes_at_level_k_1(node.left_node, level, current_level + 1)
+            BSTOperations.print_all_nodes_at_level_k_1(node.right_node, level, current_level + 1)
+
+    @staticmethod
+    def print_all_nodes_at_level_k_using_queues(node: Node, level: int = 1) -> None:
+        current_level = 1
+        bst_queue: queue = queue.Queue(0)
+        bst_queue.put(current_level)
+        bst_queue.put(node)
+        while not bst_queue.empty():
+            popped_level: int = bst_queue.get()
+            popped_node: Node = bst_queue.get()
+            if popped_node:
+                if popped_level == level:
+                    print(popped_node.value)
+                else:
+                    bst_queue.put(popped_level + 1)
+                    bst_queue.put(popped_node.left_node)
+                    bst_queue.put(popped_level + 1)
+                    bst_queue.put(popped_node.right_node)
+
+    @staticmethod
+    def print_all_nodes_at_level_k_using_queues_1(node: Node, level: int = 1) -> None:
+        if not node:
+            return
+        current_level = 1
+        bst_queue: queue = queue.Queue(0)
+        bst_queue.put(current_level)
+        bst_queue.put(node)
+        while not bst_queue.empty():
+            popped_level: int = bst_queue.get()
+            popped_node: Node = bst_queue.get()
+            if popped_level == level:
+                print(popped_node.value)
+            else:
+                if popped_node.left_node:
+                    bst_queue.put(popped_level + 1)
+                    bst_queue.put(popped_node.left_node)
+                if popped_node.right_node:
+                    bst_queue.put(popped_level + 1)
+                    bst_queue.put(popped_node.right_node)
+
 
 sample_bst = Node(100, Node(50, Node(40), Node(60)), Node(150, Node(140), Node(160)))
 
@@ -124,6 +210,7 @@ BSTOperations.insert_node(sample_bst1, 60)
 BSTOperations.insert_node(sample_bst1, 120)
 BSTOperations.insert_node(sample_bst1, 110)
 BSTOperations.insert_node(sample_bst1, 130)
+BSTOperations.insert_node(sample_bst1, 150)
 
 print("Printing In Order of a tree after inserting new nodes:")
 BSTOperations.print_in_order_of_tree(sample_bst1)
@@ -139,3 +226,21 @@ print(min_node_of_bst.value)
 print("Finding max node")
 max_node_of_bst = BSTOperations.find_max_node(sample_bst1)
 print(max_node_of_bst.value)
+
+print("Printing BST of a tree:")
+BSTOperations.print_bst_of_tree(sample_bst1)
+
+print("Printing BST of a tree-1:")
+BSTOperations.print_bst_of_tree_1(sample_bst1)
+
+print("Printing all nodes of a tree at level k:")
+BSTOperations.print_all_nodes_at_level_k(sample_bst1, 3)
+
+print("Printing all nodes of a tree at level k-1:")
+BSTOperations.print_all_nodes_at_level_k_1(sample_bst1, 3)
+
+print("Printing all nodes of a tree at level k using queues:")
+BSTOperations.print_all_nodes_at_level_k_using_queues(sample_bst1, 3)
+
+print("Printing all nodes of a tree at level k using queues-1:")
+BSTOperations.print_all_nodes_at_level_k_using_queues_1(sample_bst1, 3)
