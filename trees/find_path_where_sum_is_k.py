ffecc0db38f77368a19d224bd6a555acdb637786
Arvind Dathathri <arvind.dathathri@gmail.com>
2020-01-16 16:13:37 -0500

    Trees: Path whose sum is equal to k: SOlution will print True if a path is found and will print False if a path is not found. However, the solution will print only the first path

--- trees/find_path_where_sum_is_k.py
+++ trees/find_path_where_sum_is_k.py
@@ -10,19 +10,19 @@ class Node:
 class Solution:
     def find_path_where_sum_is_k(self, node: Node, k: int, result: List = []):
         if not node:
-            return
+            return False
         temp_result = result.copy()
         temp_result.append(node.value)
         if node.value == k and not node.left_node and not node.right_node:
             print("Path: {}".format(temp_result))
-            return
+            return True
         if node.value > k:  # no need to traverse further
-            return
-        self.find_path_where_sum_is_k(node.left_node, k - node.value, temp_result)
-        self.find_path_where_sum_is_k(node.right_node, k - node.value, temp_result)
+            return False
+        return self.find_path_where_sum_is_k(node.left_node, k - node.value, temp_result) or \
+               self.find_path_where_sum_is_k(node.right_node, k - node.value, temp_result)
 
 
-sample_tree = Node(1, Node(2, Node(4), Node(5)), Node(3, Node(6)))
-target_sum = 2
+sample_tree = Node(1, Node(2, Node(4), Node(5)), Node(3, Node(4), Node(6)))
+target_sum = 8
 a = Solution()
-a.find_path_where_sum_is_k(sample_tree, target_sum)
+print(a.find_path_where_sum_is_k(sample_tree, target_sum))
