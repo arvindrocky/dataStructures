1721e85333ebce27b659b2d1070e679c939e64d1
Arvind Dathathri <arvind.dathathri@gmail.com>
2020-01-27 14:16:11 -0500

    Strings: Added code to reverse a string

---
+++ strings/reverse_string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def __init__(self):
+        pass
+
+    def reverse_string(self, input_str:str) -> str:
+        if not input_str:
+            return input_str
+        left = 0
+        right = len(input_str) - 1
+        while left < right:
+            input_str = self.str_swap(input_str, left, right)
+            left += 1
+            right -= 1
+        return input_str
+
+    def str_swap(self, s, i, j) -> str:
+        temp = list(s)
+        temp[i], temp[j] = temp[j], temp[i]
+        return "".join(temp)
+
+
+sol = Solution()
+print(sol.reverse_string("string"))
